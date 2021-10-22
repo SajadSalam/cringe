aa3b73edb7b0d728fcfc9abe9d18ae91ed8c6e1f
Sajad Salam <31234908+SajadSalam@users.noreply.github.com>
2021-10-22 04:53:56 +0300

    s

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+    return "unknown";
+}
+var app = new Vue({
+    el: '#app',
+    data: {
+      device: window.getMobileOperatingSystem()
+    }
+  })
